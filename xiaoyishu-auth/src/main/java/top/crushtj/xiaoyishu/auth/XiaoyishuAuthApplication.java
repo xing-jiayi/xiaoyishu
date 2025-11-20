86dc2007875e3cb9c7a1c259dab3620530f3ae3a
ayi <2294931964@qq.com>
2025-11-20 14:16:51 +0800

    基础模块初始化

---
+++ xiaoyishu-auth/src/main/java/top/crushtj/xiaoyishu/auth/XiaoyishuAuthApplication.java
@@ -0,0 +1,13 @@
+package top.crushtj.xiaoyishu.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoyishuAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaoyishuAuthApplication.class, args);
+	}
+
+}
---
+++ xiaoyishu-auth/src/test/java/top/crushtj/xiaoyishu/auth/XiaoyishuAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package top.crushtj.xiaoyishu.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoyishuAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
