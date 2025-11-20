ae8d28244b5ad123077a091e67a35db2f33746b7
ayi <2294931964@qq.com>
2025-11-20 16:33:17 +0800

    修改认证模块主类名

--- xiaoyi-auth/src/main/java/top/crushtj/xiaoyishu/auth/XiaoyiAuthApplication.java
+++ xiaoyi-auth/src/main/java/top/crushtj/xiaoyishu/auth/XiaoyiAuthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XiaoyishuAuthApplication {
+public class XiaoyiAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(XiaoyishuAuthApplication.class, args);
+		SpringApplication.run(XiaoyiAuthApplication.class, args);
 	}
 
 }
