f921bad9fc191403ceebce260081aef01e541184
ayi <2294931964@qq.com>
2025-11-27 19:28:07 +0800

    整合Mubatis-Plus

--- xiaoyi-auth/src/test/java/top/crushtj/xiaoyishu/auth/XiaoyiAuthApplicationTests.java
+++ xiaoyi-auth/src/test/java/top/crushtj/xiaoyishu/auth/XiaoyiAuthApplicationTests.java
@@ -1,13 +1,34 @@
 package top.crushtj.xiaoyishu.auth;
 
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import top.crushtj.xiaoyishu.auth.domain.entity.UserEntity;
+import top.crushtj.xiaoyishu.auth.domain.mappers.UserMapper;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
+@Slf4j
 class XiaoyiAuthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Resource
+    UserMapper userMapper;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testInsert() {
+        UserEntity user = UserEntity.builder()
+                                    .username("刑加一")
+                                    .createTime(LocalDateTime.now())
+                                    .updateTime(LocalDateTime.now())
+                                    .build();
+
+        userMapper.insert(user);
+    }
 
 }
