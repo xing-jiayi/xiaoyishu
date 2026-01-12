1c4a093b46e8f3175b09f366da8027ca8eee18c2
hanfuye <hanfuye@sinosoft.com.cn>
2026-01-12 19:00:06 +0800

    添加Sa-Token配置并简单测试

--- xiaoyi-auth/src/main/java/com/jy/xiaoyishu/auth/controller/TestController.java
+++ xiaoyi-auth/src/main/java/com/jy/xiaoyishu/auth/controller/TestController.java
@@ -3,12 +3,18 @@ package com.jy.xiaoyishu.auth.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.jy.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.jy.framework.common.response.Response;
 import com.jy.xiaoyishu.auth.vo.User;
 
+import cn.dev33.satoken.stp.StpUtil;
+
 import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  *
@@ -31,6 +37,22 @@ public class TestController {
     @ApiOperationLog(description = "测试接口2")
     public Response<User> test2(@RequestBody User user) {
         return Response.success(user);
+    }
+
+    @RequestMapping("/user/doLogin")
+    public String doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if("ayi".equals(username) && "12345678".equals(password)) {
+            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:8080/user/isLogin
+    @RequestMapping("/user/isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
     }
 
 }
